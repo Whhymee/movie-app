ccd0843b0054741c2fdfce669fcbefe87e7316ee
RAJ <raj@PhonePes-MacBook-Pro.local>
2018-01-09 16:55:47 +0530

    Added Nav and loading component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Nav from './Nav';
+import Loading from './Loading';
+
+const Router = BrowserRouter;
 
 class App extends Component {
 	render() {
 		return (
+      <Router>
 			<div>
-				<h1>Movie App</h1>
+        <Nav />
+        <Loading />
 			</div>
+      </Router>
 		);
 	}
 }
